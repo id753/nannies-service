9ef5e2dee3f172e58542aab5cb3439bcf3edf2e8
Eugene O. (id753) <mimywhy@gmail.com>
2026-04-24 16:06:37 +0300

    Change Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,62 +1,96 @@
 "use client";
 
 import Link from "next/link";
-import css from "./Header.module.css";
 import { useState } from "react";
-import Modal from "../Modal/Modal";
 import { usePathname } from "next/navigation";
+
+import css from "./Header.module.css";
+import Modal from "../Modal/Modal";
+import { UserIcon } from "../Icons/Icons";
 
 const Header = () => {
   const [modalType, setModalType] = useState<"login" | "register" | null>(null);
 
-  const closeModal = () => setModalType(null);
+  const pathname = usePathname() ?? "";
 
-  const pathname = usePathname();
   const isHome = pathname === "/";
+  const isNannies = pathname.startsWith("/nannies");
+  const isFavorites = pathname.startsWith("/favorites");
+
+  const isLoggedIn = false;
+  // const isLoggedIn = true;
+  const userName = "Ilona";
+
   const headerClasses = `${css.header} ${isHome ? css.homePage : css.colored}`;
+
+  const Nav = (
+    <ul className={css.navigation}>
+      <li>
+        <Link href="/" className={isHome ? css.active : ""}>
+          Home
+        </Link>
+      </li>
+
+      <li>
+        <Link href="/nannies" className={isNannies ? css.active : ""}>
+          Nannies
+        </Link>
+      </li>
+
+      {isLoggedIn && (
+        <li>
+          <Link href="/favorites" className={isFavorites ? css.active : ""}>
+            Favorites
+          </Link>
+        </li>
+      )}
+    </ul>
+  );
+
+  const AuthBlock = isLoggedIn ? (
+    <div className={css.userMenu}>
+      <div className={css.userAvatar}>
+        <UserIcon />
+      </div>
+      <span className={css.userName}>{userName}</span>
+      <button className={css.logoutBtn}>Log out</button>
+    </div>
+  ) : (
+    <div className={css.authButtons}>
+      <button className={css.loginBtn} onClick={() => setModalType("login")}>
+        Log In
+      </button>
+      <button className={css.regBtn} onClick={() => setModalType("register")}>
+        Registration
+      </button>
+    </div>
+  );
 
   return (
     <header className={headerClasses}>
       <div className={css.container}>
-        {/* Левая часть: Логотип */}
-        <div className={css.leftSide}>
-          <Link href="/" className={css.logo} aria-label="Home">
-            Nanny.Services
-          </Link>
-        </div>
+        {/* LOGO */}
+        <Link href="/" className={css.logo}>
+          Nanny.Services
+        </Link>
 
-        {/* Правая часть: Навигация и Кнопки */}
-        <div className={css.rightSide}>
-          <nav aria-label="Main Navigation">
-            <ul className={css.navigation}>
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/nannies">Nannies</Link>
-              </li>
-            </ul>
-          </nav>
-
-          <div className={css.authButtons}>
-            <button
-              onClick={() => setModalType("login")}
-              className={css.loginBtn}
-            >
-              Log In
-            </button>
-
-            <button
-              onClick={() => setModalType("register")}
-              className={css.regBtn}
-            >
-              Registration
-            </button>
+        {/* NAV */}
+        {isHome ? (
+          <div className={css.rightSide}>
+            <nav className={css.nav}>{Nav}</nav>
+            {AuthBlock}
           </div>
-        </div>
+        ) : (
+          <>
+            <nav className={css.navDefault}>{Nav}</nav>
+            <div className={css.rightSide}>{AuthBlock}</div>
+          </>
+        )}
       </div>
 
-      {modalType && <Modal type={modalType} onClose={closeModal} />}
+      {modalType && (
+        <Modal type={modalType} onClose={() => setModalType(null)} />
+      )}
     </header>
   );
 };
