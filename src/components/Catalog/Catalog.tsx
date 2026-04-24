c42986cf9320eb43b5ff28beacca0ff5d187ef3d
Eugene O. (id753) <mimywhy@gmail.com>
2026-04-24 12:42:51 +0300

    Change Filter style

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,10 +1,11 @@
 "use client";
-
+import css from "./Catalog.module.css";
 import { useState, useEffect } from "react";
 import { ref, onValue } from "firebase/database";
 import { db } from "../../firebaseConfig";
 import Item from "../Item/Item";
 import { Babysitter } from "@/src/types";
+import Filter from "../Filter/Filter";
 
 const NanniesList = () => {
   const [nannies, setNannies] = useState<Babysitter[]>([]);
@@ -35,11 +36,14 @@ const NanniesList = () => {
   if (loading) return <div>Searching for nannies...</div>;
 
   return (
-    <ul style={{ listStyle: "none", padding: 0 }}>
-      {nannies.map((item, index) => (
-        <Item key={index} item={item} />
-      ))}
-    </ul>
+    <div className={css.container}>
+      <Filter />
+      <ul className={css.list}>
+        {nannies.map((item, index) => (
+          <Item key={index} item={item} />
+        ))}
+      </ul>
+    </div>
   );
 };
 
---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,8 @@
+import css from "./Filter.module.css";
+import React from "react";
+
+const Filter = () => {
+  return <div>Filter</div>;
+};
+
+export default Filter;
