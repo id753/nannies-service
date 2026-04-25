253c90a83207312d61115221df68a771aab04948
Eugene O. (id753) <mimywhy@gmail.com>
2026-04-25 15:33:46 +0300

    Fix buttons style

--- src/utils/generateTimeOptions.ts
+++ src/utils/generateTimeOptions.ts
@@ -1,13 +1,12 @@
 export const generateTimeOptions = () => {
-  const times = [];
-  for (let hour = 9; hour <= 18; hour++) {
-    // Диапазон с 09:00 до 18:00
+  const times = ["Meeting time"];
+
+  for (let hour = 0; hour <= 23; hour++) {
     const h = hour.toString().padStart(2, "0");
-    times.push(`${h}:00`);
-    if (hour !== 18) {
-      // Чтобы не добавлять 18:30, если работа до 18:00
-      times.push(`${h}:30`);
-    }
+
+    times.push(`${h}  :  00`);
+    times.push(`${h}  :  30`);
   }
+
   return times;
 };
