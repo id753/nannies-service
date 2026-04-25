39a1d870ccd59626aee1602595d21333b7789ac2
Eugene O. (id753) <mimywhy@gmail.com>
2026-04-25 11:27:35 +0300

    Add firstLetter Fn

--- src/components/ReviewCard/ReviewCard.tsx
+++ src/components/ReviewCard/ReviewCard.tsx
@@ -13,11 +13,12 @@ interface ReviewCardProps {
 }
 
 const ReviewCard = ({ review }: ReviewCardProps) => {
+  const firstLetter = review.reviewer[0]?.toUpperCase() || "?";
   return (
     <li className={css.container}>
       <div className={css.containerFlex}>
         <div className={css.avatarContainer}>
-          <span className={css.avatarLetter}>G</span>
+          <span className={css.avatarLetter}>{firstLetter}</span>
         </div>
         <div className={css.DescripContainer}>
           <h4 className={css.name}>{review.reviewer}</h4>
