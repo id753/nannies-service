24eddb4b4ad8669bce7dedb00844ae3f4cec70fd
Eugene O. (id753) <mimywhy@gmail.com>
2026-05-06 13:11:02 +0300

    Fix NannyCounter

--- src/components/NannyCounter/NannyCounter.tsx
+++ src/components/NannyCounter/NannyCounter.tsx
@@ -1,13 +1,29 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+import { useRef, useState, useEffect } from "react";
 import { useCounter } from "../../hooks/Counter";
+
 interface CounterProps {
   start?: number;
   to: number;
   duration?: number;
 }
-export const NannyCounter = ({ start = 0, to, duration = 4 }: CounterProps) => {
-  const rounded = useCounter(start, to, duration);
-  return <motion.span>{rounded}</motion.span>;
+
+export const NannyCounter = ({
+  start = 14621,
+  to,
+  duration = 4,
+}: CounterProps) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.5 });
+  const [hasStarted, setHasStarted] = useState(false);
+
+  useEffect(() => {
+    if (isInView) setHasStarted(true);
+  }, [isInView]);
+
+  const value = useCounter(start, hasStarted ? to : start, duration);
+
+  return <motion.span ref={ref}>{value}</motion.span>;
 };
